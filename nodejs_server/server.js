d4d1d3339ead7af69418364653c40695bbcd6715
alonbarak-dev <alonbarak3110@gmail.com>
2023-07-11 22:07:24 +0300

    Elastic search added to node.js server

--- nodejs_server/server.js
+++ nodejs_server/server.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 
+// elastic search 
+const config = require('config')
+const elasticConfig = config.get('elastic')
+
+const { Client } = require('elasticsearch');
+// Create a client instance
+const client = new Client({
+             node: 'http://35.234.119.103:5601',
+             auth:{
+              username: elasticConfig.username, 
+              passward: elasticConfig.passward
+             } })
+
+
 const app = express()
 const port = 3000
 app.use(bodyParser.json());
@@ -21,6 +35,19 @@ app.get('/', (req, res) => {
     res.send("Hello World!")
 })
 
+async function insertData(indexName, data) {
+  try {
+    const response = await client.index({
+      index: indexName,
+      body: data
+    });
+
+    console.log('Data inserted:', response);
+  } catch (error) {
+    console.error('Error inserting data:', error);
+  }
+}
+
 
 app.post('/simdata', (req, res) => {
     const { Date, notfac, loc, type, urg } = req.body;
@@ -38,3 +65,14 @@ app.post('/simdata', (req, res) => {
 app.listen(port, () => {
     console.log(`server listening on port ${port}`)
 })
+
+
+
+// Usage example
+const indexName = 'event_test_idx';
+const data = {
+  field1: 'value1',
+  field2: 'value2'
+};
+
+insertData(indexName, data);
