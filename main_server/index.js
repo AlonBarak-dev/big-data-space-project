527cd72d30b325bf338c679d59a2b81ab58588b1
alonbarak-dev <alonbarak3110@gmail.com>
2023-07-26 17:51:48 +0300

    added a function that get the NEOs from the redis cache or Disk

--- main_server/index.js
+++ main_server/index.js
@@ -4,15 +4,17 @@ const path = require('path');
 const axios = require("axios");
 const bodyParser = require('body-parser');
 const { Client } = require('@elastic/elasticsearch'); // elastic search
+const Redis = require('ioredis');   // redis
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 const KEY = "DdYnXnHGhGOgBhdoKoIvo5IyprK7EKfqiZtmKrjo";
 const neo_url = "https://api.nasa.gov/neo/rest/v1/feed";
-const indexName = 'simulator_events'; // Elastic search - index name for simulator events
+const simIndexName = 'simulator_events'; // Elastic search - index name for simulator events
+const neoIndexName = 'neos'
 
-// Create a client instance
+// Create a new Elastic client instance
 const client = new Client({
   node: `http://35.234.119.103:9200`,
   auth: {
@@ -20,6 +22,13 @@ const client = new Client({
     password: 'changeme'
   }
 })
+
+// Create a new Redis client instance
+const redis = new Redis({
+  host: '35.234.119.103', // Redis server host
+  port: 6379,        // Redis server port
+});
+
 
 // Serve static files from the React build directory
 app.use(express.static(path.join(__dirname, 'build')));
@@ -32,12 +41,12 @@ app.get("/get_event_list", async (req, res) => {
   let results;
 
   if (queryValue) {
-    results = await searchDocuments(indexName, queryValue);
+    results = await searchDocuments(simIndexName, queryValue);
     console.log(results);
     console.log("Searching For: ", queryValue);
   } else {
     console.log("Searching for all enteries")
-    results = await getAllEntries(indexName);
+    results = await getAllEntries(simIndexName);
   }
 
   const events = results.hits.hits.map((hit) => {
@@ -77,6 +86,62 @@ async function getAllEntries(indexName) {
   });
   return response;
 }
+
+
+// Neos using Redis (Cache) & Elastic/MongoDB ((Disk)
+
+app.get("/get_neos_from_db", async (req, res) => {
+  const key = "neo_list"
+  try {
+    const value = await getValueFromRedisOrElasticsearch(key);
+    res.json({ key, value });
+  } catch (err) {
+    res.status(500).json({ error: "An error occurred while fetching data." });
+  }
+})
+
+async function getValueFromCacheOrDisk(key) {
+  // Try to get the value from Redis
+  return new Promise((resolve, reject) => {
+    redisClient.get(key, async (err, value) => {
+      if (err) {
+        console.error('Redis GET Error:', err);
+        reject(err);
+        return;
+      }
+      // If the value exists in Redis, resolve with it
+      if (value !== null) {
+        resolve(value);
+      } 
+      else {  // TODO fit for elastic when Eyal is done!
+        try {
+          // If the value does not exist in Redis, search in Elasticsearch
+          const { body } = await elasticClient.search({
+            index: neoIndexName, // Replace with your Elasticsearch index
+            body: {
+              query: {
+                term: {
+                  key: key, // Replace 'key' with the actual field name in your Elasticsearch documents
+                },
+              },
+            },
+          });
+
+          // Assuming it is a LIST 
+          const neoList = body.hits.hits.map((hit) => {
+            return hit["_source"].value // TODO - Replace 'value' with the actual field name containing the value
+          })
+          resolve(neoList);
+        } 
+        catch (err) {
+          console.error('Elasticsearch Error:', err);
+          reject(err);
+        }
+      }
+    });
+  });
+}
+
 
 app.get("/get_neos", (req, res) => {
   const end_date = new Date();
