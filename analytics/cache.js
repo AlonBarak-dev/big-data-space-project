079b168c74f8eb81b844c3f193949def72f84fc2
yurigrigorian <ygrigorian@perforce.com>
2023-07-30 14:36:37 +0300

    analytics updated cache

--- analytics/cache.js
+++ analytics/cache.js
@@ -1,17 +1,18 @@
-let redis = require('redis');
-const {REDIS_HOST} = require("./config");
+const {REDIS_CONFIG} = require("./config");
 const {createClient} = require("redis");
-let client = createClient({
-    url: `redis://${REDIS_HOST}:6379`
-});
+let client = createClient(REDIS_CONFIG);
 
 client.on('error', err => console.log('Redis Client Error', err));
 
 client.connect();
 
+const DEFAULT_TTL_SECS = 8*60*60;
+const CACHE_TAG = 'ML';
+
 const getKey =  (suffix) => {
-    return "CACHE::ML::" + suffix
+    return `CACHE::${CACHE_TAG}::` + suffix
 }
+
 module.exports = {
     getKey: getKey,
     getCached: async (req, res, next) => {
@@ -19,19 +20,16 @@ module.exports = {
         console.log(key);
         const c =  await client.get(key);
         if(c){
+            console.log("Got from cache.");
             res.json(JSON.parse(c));
         } else{
             try{
                 next();
-
-            }catch {
-
-            }
+            }catch {}
         }
-
     },
     caching: async (key, data) => {
-        await client.set(key, JSON.stringify(data))
+        await client.set(key, JSON.stringify(data), {'EX':DEFAULT_TTL_SECS})
     },
     delCache: async (key) => {
         await client.del(key)
--- analytics/config.js
+++ analytics/config.js
@@ -2,5 +2,9 @@ const BIGML_USERNAME="gryuri93"
 const BIGML_API_KEY="d859eafabf579c56bafd9ff43a46c31cac0a20b5"
 const MONGO_CONN_STRING="mongodb+srv://big-data-space:Aa123456@big-data-space.wlxmqwy.mongodb.net/?retryWrites=true&w=majority"
 const MONGO_DB='big-data';
-const REDIS_HOST = '35.234.119.103'
-module.exports = {BIGML_API_KEY, BIGML_USERNAME, MONGO_CONN_STRING, MONGO_DB,REDIS_HOST}
+const REDIS_CONN = "redis://35.234.119.103:6379";
+
+const REDIS_CONFIG = {
+    url: REDIS_CONN
+}
+module.exports = {BIGML_API_KEY, BIGML_USERNAME, MONGO_CONN_STRING, MONGO_DB,REDIS_CONFIG}
